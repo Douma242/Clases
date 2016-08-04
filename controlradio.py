e81b6878a1258002a9c8b02aaca9c51fdcbbbc84
Douma242 <danieldouma@hotmail.es>
2016-08-04 09:48:40 -0600

    :(

--- controlradio.py
+++ controlradio.py
@@ -6,6 +6,8 @@ while DeseaContinuar:
 		op=int(input("1.Apagar \n2.Subir/Bajar Volumen \n3.Subir/Bajar Emisora \n4.AM/FM \n"))
 		if op==1:
 			Mradio.encendido=False	
+		if op==4:
+			Mradio.en_FM=cambio_emisora(Mradio)
 	else:
 		opc=int(input("1.Encender \n2.Salir \n"))
 		if opc==1:
@@ -13,3 +15,4 @@ while DeseaContinuar:
 		else:
 			DeseaContinuar=False
 print("Bai Bai")
+print(Mradio.en_FM)
